bee0fce691e3c696f0a896761880bf0237f1fc04
Anne Sarnack <anne.sarnack1@bp.com>
2019-04-29 14:42:52 -0400

    forgot some packages, also starting to fill in test methods

--- SeleniumDemo/UITests/WebTestsSteps.cs
+++ SeleniumDemo/UITests/WebTestsSteps.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace UITests
 {
     [Binding]
     public class WebTestsSteps
     {
+        private IWebDriver _webDriver = null;
+
         [Given(@"I am on Google")]
         public void GivenIAmOnGoogle()
         {
-            ScenarioContext.Current.Pending();
+            _webDriver.Navigate().GoToUrl("www.google.com");
         }
         
         [When(@"I search for weather")]
@@ -22,6 +26,18 @@ namespace UITests
         public void ThenTheWeatherShouldDisplay()
         {
             ScenarioContext.Current.Pending();
+        }
+
+        [BeforeScenario]
+        public void CreateTestDriver()
+        {
+            _webDriver = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void DisposeOfWebDriver()
+        {
+            _webDriver.Dispose();
         }
     }
 }
